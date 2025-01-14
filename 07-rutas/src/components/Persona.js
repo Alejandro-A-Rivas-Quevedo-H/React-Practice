23c9c166d76055bb59d3315f23d807103e7f905f
Alejandro-A-Rivas-Quevedo-H <00314143alex@gmail.com>
2025-01-13 20:30:59 -0600

    se le puso if

--- 07-rutas/src/components/Persona.js
+++ 07-rutas/src/components/Persona.js
@@ -1,26 +1,45 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Persona = () => {
+  const { nombre, apellido } = useParams();
+  const navegar = useNavigate();
 
+  const enviar = (e) => {
+    e.preventDefault();
 
-    const {nombre, apellido} = useParams();
+    
+    const nombre = e.target.elements.nombre.value;
+    const apellido = e.target.elements.apellido.value;
 
+    const url = `/persona/${nombre}/${apellido}`;
+
+    if(nombre.length <= 0 && apellido.length <= 0) {
+      navegar('/inicio');
+    } else if (nombre === "contacto") {
+      navegar('/contacto');
+    } else {
+      navegar(url);
+    }
+    
+  };
 
   return (
     <div>
-      {!nombre && <h1>No hay ninguna persona que mostrar</h1>}
-      <h1>pagina de Persona : {nombre} {apellido}</h1>
-      <p>esta es la pagina de persona</p>
+      {!nombre && !apellido ? (
+        <h1>No hay ninguna persona que mostrar</h1>
+      ) : (
+        <h1>Página de Persona: {nombre} {apellido}</h1>
+      )}
+      <p>Esta es la página de persona</p>
 
-      <form>
-        <input type="text" placeholder="nombre" />
-        <input type="text" placeholder="apellido" />
-        <button type="submit" name="enviar" value="enviar">Enviar</button>
+      <form onSubmit={enviar}>
+        <input type="text" name="nombre" placeholder="Nombre" required />
+        <input type="text" name="apellido" placeholder="Apellido" required />
+        <button type="submit">Enviar</button>
       </form>
-
     </div>
-  )
-}
+  );
+};
 
-export default Persona
+export default Persona;

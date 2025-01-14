0070797bb2e135684d159ca47b84b329c8ea8d1c
Alejandro-A-Rivas-Quevedo-H <00314143alex@gmail.com>
2025-01-13 18:15:20 -0600

    se agrego input en persona

--- 07-rutas/src/components/Persona.js
+++ 07-rutas/src/components/Persona.js
@@ -12,6 +12,13 @@ const Persona = () => {
       {!nombre && <h1>No hay ninguna persona que mostrar</h1>}
       <h1>pagina de Persona : {nombre} {apellido}</h1>
       <p>esta es la pagina de persona</p>
+
+      <form>
+        <input type="text" placeholder="nombre" />
+        <input type="text" placeholder="apellido" />
+        <button type="submit" name="enviar" value="enviar">Enviar</button>
+      </form>
+
     </div>
   )
 }
